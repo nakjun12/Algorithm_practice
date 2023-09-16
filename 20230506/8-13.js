84039e28be6550dede7af51495e97890a51f9891
nakjun12 <wns450@gmail.com>
2023-09-16 12:22:13 +0900

    Update 8-13.js

--- 20230506/8-13.js
+++ 20230506/8-13.js
@@ -1,45 +1,13 @@
 function solution(N, SUM) {
-  let dy = Array.from(Array(N + 1), () => Array(N + 1).fill(0));
-  let b = Array.from({ length: N }, () => 0);
-  let answer;
-  let ch = Array.from({ length: N + 1 }, () => 0);
-  let a = Array.from({ length: N }, () => 0);
-  function combi(n, r) {
-    // nCr의 n을 고정하고 r의 값들을 차례대로 구함
+  let dy = Array.from({ length: N + 1 }, Array(N + 1).fill(0));
+
+  function combi() {
     //조합 메모이제이션
-    if (dy[n][r] > 0) return dy[n][r];
-    if (n === r || r === 0) {
-      return 1;
-    } else {
-      return (dy[n][r] = combi(n - 1, r - 1) + combi(n - 1, r));
-    }
   }
-  let flag = 0;
-  function DFS(n, sum) {
+
+  function DFS() {
     //순열
-    if (flag) return;
-    if (n === N && sum === SUM) {
-      answer = a.slice();
-      flag = 1;
-    } else {
-      for (let i = 1; i <= N; i++) {
-        if (ch[i] === 0) {
-          ch[i] = 1;
-          a[n] = i;
-          DFS(n + 1, sum + i * b[n]);
-          ch[i] = 0;
-        }
-      }
-    }
   }
-
-  for (let i = 0; i < N; i++) {
-    b[i] = combi(N - 1, i); // 4개면 3C0부터 시작임
-  }
-
-  DFS(0, 0); //주의
-
-  return answer;
 }
 
 console.log(solution(4, 16));
